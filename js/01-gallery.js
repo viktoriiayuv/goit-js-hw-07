abe17447515432440dc937d15ab630aae6d45d90
Viktoriia Bieltsova <vikav0206@gmail.com>
2023-01-16 23:58:04 +0200

    update main files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryItemsMarkup = createGallaryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+galleryEl.addEventListener("click", onGalleryItemClick);
+
+function createGallaryMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+		})
+		.join("");
+}
+
+function onGalleryItemClick(event) {
+	event.preventDefault();
+
+	if (event.target.nodeName !== "IMG") {
+		return;
+	}
+
+	const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+
+	instance.show();
+
+	document.addEventListener("keydown", event => {
+		if (event.code === "Escape") {
+			instance.close();
+		}
+	});
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryItemsMarkup = createGallaryMarkup(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+let gallery = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
+
+function createGallaryMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+		})
+		.join("");
+}
